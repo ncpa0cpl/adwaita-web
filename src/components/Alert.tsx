759b3d3fbb184f07bd84972f350b9dbd73ee183b
ncpa0cpl <sbretner@touchbistro.com>
2022-07-26 16:09:22 +0200

    fix: incorrect alert props

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,40 +8,40 @@ import { Button } from "./Button";
 
 export type AlertProps = React.PropsWithChildren<{
   /** The title/header of the alert. */
-  title: string;
+  title?: string;
   /** Class names that will be added to the alert container. */
-  className: string;
+  className?: string;
   /** Size of the alert. */
-  size: "mini" | "small" | "medium" | "large" | "huge" | "mega";
+  size?: "mini" | "small" | "medium" | "large" | "huge" | "mega";
   /**
    * Icon that will be displayed to the left of the alert message. If not provided a
    * default icon will be used depending of the alert type.
    */
-  icon: AdwaitaIcon;
+  icon?: AdwaitaIcon;
   /** Wether to show a close button in the alert. */
-  showClose: boolean;
+  showClose?: boolean;
   /** A callback that will be invoked when a close button is clicked. */
-  onClose: () => void;
+  onClose?: () => void;
   /**
    * When set to true the type of the alert will be set to "Information Message",
    * Information Alerts have a bulb icon and a blue background.
    */
-  info: boolean;
+  info?: boolean;
   /**
    * When set to true the type of the alert will be set to "Success Message", Success
    * Alerts have a checkmark icon and a green background.
    */
-  success: boolean;
+  success?: boolean;
   /**
    * When set to true the type of the alert will be set to "Warning Message", Warning
    * Alerts have a warning icon and a yellow background.
    */
-  warning: boolean;
+  warning?: boolean;
   /**
    * When set to true the type of the alert will be set to "Danger Message", Danger
    * Alerts have a warning icon and a red background.
    */
-  danger: boolean;
+  danger?: boolean;
 }>;
 
 /** Alert component for displaying messages to the user in a way that attracts attention. */
